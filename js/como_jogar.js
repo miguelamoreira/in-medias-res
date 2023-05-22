b66ca1eab1b3c2e9b758d19f7cfa8928d4237d66
miguelamoreira <40210256@esmad.ipp.pt>
2023-05-22 19:35:15 +0100

    responsividade leaderboard, como jogar + mudança da tipografia

--- js/como_jogar.js
+++ js/como_jogar.js
@@ -1,18 +1,15 @@
 const btnArrow = document.getElementById('btnScroll');
 const imgBtn = document.querySelector('.arrow');
-const firstTxt = document.querySelector('.visible');
-const secondTxt = document.querySelector('.invisible');
+const info = document.querySelector('#card-info');
 let isArrow1 = true;
 
 btnArrow.addEventListener('click', function() {
     if (isArrow1) {
-        firstTxt.className = 'invisible';
-        secondTxt.className = 'visible';
+        info.innerHTML = 'Simultaneamente, tenta ajudar o escuteiro preferido da Pixar, o Russel, a encontrar os seus pins. Para além disso, procura resolver os desafios o mais rapidamente possível para poderes aparecer na leaderboard!'
         imgBtn.src = '../assets/arrow2.png';
         isArrow1 = false; 
     } else {
-        firstTxt.className = 'visible';
-        secondTxt.className = 'invisible';
+        info.innerHTML = 'Tenta escapar do universo cinematográfico da Disney e da Pixar através da resolução de desafios. Explora as salas de modo a encontrares os desafios nelas contidas e a aprenderes mais sobre estas produtoras e os seus projetos.'
         imgBtn.src = '../assets/arrow1.png';
         isArrow1 = true; 
     }
