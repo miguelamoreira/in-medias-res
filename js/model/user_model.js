3114b3d7bb498b79cce841d480b6eabd01bbeb3a
miguelamoreira <40210256@esmad.ipp.pt>
2023-05-23 14:04:47 +0100

    organização de ficheiros

---
+++ js/model/user_model.js
@@ -0,0 +1,60 @@
+// código baseado no exemplo BandsApp feito em aula
+
+let users;
+
+export function init() {
+    users = localStorage.users ? JSON.parse(localStorage.users) : [];
+}
+
+export function add(username, email, password, password2) {
+    if (users.some((user) => user.username === username)) {
+        throw Error (`O username ${username} já se encontra a ser utilizado!`);
+    } else if (users.some((user) => user.email === email)) {
+        throw Error (`O email ${email} já se encontra a ser utilizado!`);
+    } else if (users.some((user) => user.email === email)) {
+        throw Error ('Palavra-passe incorreta!');
+    } else if (password != password2) {
+        throw Error ('As palavras-passe não correspondem!')
+    } else {
+        users.push(new User(username, password));
+        localStorage.setItem("users", JSON.stringify(users));
+    }
+}
+
+export function login(username, password) {
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
+    if (user) {
+      sessionStorage.setItem("loggedUser", JSON.stringify(user));
+      return true;
+    } else {
+      throw Error("Invalid login!");
+    }
+  }
+
+export function logout() {
+    sessionStorage.removeItem("loggedUser");
+}
+  
+export function isLogged() {
+    return sessionStorage.getItem("loggedUser") ? true : false;
+}
+  
+export function getUserLogged() {
+    return JSON.parse(sessionStorage.getItem("loggedUser"));
+}
+
+class user {
+    username = '';
+    email = '';
+    password = '';
+    password2 = '';
+
+    constructor(username, email, password, password2) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.password2 = password2;
+    }
+}
