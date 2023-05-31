2ffd2094dbccfb2aa46ce05c00a79e2216d1ea85
sofiapcabral <s.sofia.c.cabral@gmail.com>
2023-05-31 17:26:58 +0100

    ligações menu + salas

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+document.querySelector("#btnLogin").addEventListener("click", () => {
+    location.href = "inicio_sessao.html";
+  })
+  
+  document.querySelector("#btnTutorial").addEventListener("click", () => {
+    location.href = "como_jogar.html";
+  })
+  
+  document.querySelector("#btnLeaderboard").addEventListener("click", () => {
+    location.href = "leaderboard.html";
+  })
+  
+  document.querySelector("#btnPlay").addEventListener("click", () => {
+    location.href = "salas.html";
+  })
+  
---
+++ js/view/rooms_view.js
@@ -0,0 +1,5 @@
+$(document).ready(function(e) {
+    $('img[usemap]').rwdImageMaps(); 
+    //Allows image maps to be used in a responsive design by recalculating the area coordinates 
+    // to match the actual image size on load and window.resize
+});
