79c753c7bc6d1aa387a3976e293cf3df393b9f30
miguelamoreira <40210256@esmad.ipp.pt>
2023-06-08 20:46:02 +0100

    desafios expositivos

--- js/view/rooms_view.js
+++ js/view/rooms_view.js
@@ -1,5 +1,55 @@
 $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps(); 
-    //Allows image maps to be used in a responsive design by recalculating the area coordinates 
-    // to match the actual image size on load and window.resize
+    
 });
+
+// sala 2
+const castelo = document.getElementById('castelo').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const posterIncredibles = document.getElementById('posterIncredibles').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const pocao = document.getElementById('pocao').addEventListener('click', () => {
+  alert('pin');
+})
+
+const balao = document.getElementById('balao').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const faisca = document.getElementById('faisca').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const porta2 = document.getElementById('porta').addEventListener('click', () => {
+  alert('porta');
+})
+
+//  sala 1
+const posterPixar = document.getElementById('posterPixar').addEventListener('click', () => {
+  alert('pin');
+})
+
+const posterDisney = document.getElementById('posterDisney').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const bau = document.getElementById('bau').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const desenho = document.getElementById('desenho').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
+const porta = document.getElementById('porta').addEventListener('click', () => {
+  alert('porta');
+})
+
+const bola = document.getElementById('bola').addEventListener('click', () => {
+  alert('desafio avaliativo');
+})
+
